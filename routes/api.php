1b84dcf92aabb2b269a3d7b4b716aa434c82f7eb
Yeremia Ndaumanu <yerind14@gmail.com>
2023-10-18 23:46:30 +0800

    Make model, controller, routes api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('orders', 'OrderController');
+Route::resource('products', 'ProductController');
+Route::resource('flash-sales', 'FlashSaleController');
